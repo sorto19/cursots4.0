894dcedee70f1725ba1b1eb4c8ad01c42fac0ecd
RamonSorto <ramonsorto1@gmail.com>
2020-09-19 17:13:23 -0600

    interfaces

--- interfaces_indizadas.ts
+++ interfaces_indizadas.ts
@@ -12,3 +12,41 @@ function sort (c:Comparator) {
  let out = c("first, second")
  console.log(`Ordenando con el criterio ${c.algorithm}: ${out}`)
 }
+interface Windows {
+   accountID: number
+}
+function User (w: Windows) {
+  w.accountID
+}
+interface Geometria {
+ lados : number
+ pintar(): void
+}
+interface Triangulo extends Geometria {
+ base: number
+ altura: number
+}
+interface Cuadrado extends Geometria {
+  lado : number
+}
+function procesar (g: Geometria) {
+ if(g.lados == 4){
+  let cuadrado = g as Cuadrado
+  cuadrado.lado
+  }else if (g.lados == 3) {
+  let triangulo = g as Triangulo
+  triangulo.altura
+  }
+}
+/* instanceof  con interface no se puede utilizar sirve para vcomprobar si es coincreta las guar54das*/
+class Coche {
+ fabricante: string = "honda"
+}
+class CocheConcreto implements Coche {
+ fabricante = 'honda'
+ move() {
+ }
+ }
+ let c : Coche = new Coche()
+ console.log(c instanceof Coche)
+ console.log(c instanceof CocheConcreto)
